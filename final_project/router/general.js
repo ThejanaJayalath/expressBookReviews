1b75692df9f754ae5dd78c148725c0db43af2ff5
Thejana Jayalath <thejanashehan.com@gmail.com>
2026-01-17 01:22:02 -0500

    Task 3: Get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,10 +27,23 @@ public_users.get('/isbn/:isbn', function (req, res) {
   });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    let result = {};
+  
+    // Get all keys (ISBNs) from books object
+    Object.keys(books).forEach((isbn) => {
+      if (books[isbn].author.toLowerCase() === author.toLowerCase()) {
+        result[isbn] = books[isbn];
+      }
+    });
+  
+    if (Object.keys(result).length > 0) {
+      return res.status(200).send(JSON.stringify(result, null, 4));
+    } else {
+      return res.status(404).json({ message: "No books found for this author" });
+    }
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
