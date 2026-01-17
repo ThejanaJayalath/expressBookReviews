d931cfebef329cb1287cebc8d1ceff8bdf8a3cbf
Thejana Jayalath <thejanashehan.com@gmail.com>
2026-01-17 02:25:52 -0500

    Task 8: Review added

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -44,11 +44,21 @@ regd_users.post("/login", (req, res) => {
 });
 
 
-// Add a book review
+// Add or modify a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const isbn = req.params.isbn;
+    const review = req.query.review;
+    const username = req.session.authorization.username;
+  
+    if (!books[isbn]) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  
+    books[isbn].reviews[username] = review;
+  
+    return res.status(200).json({ message: "Review added/updated successfully" });
+  });
+  
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
